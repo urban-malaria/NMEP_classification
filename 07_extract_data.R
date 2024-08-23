eda4ce1bab4603bcb4daee0cca63a6ace6514790
hephzibahadeniji <hephzibah.adeniji@gmail.com>
2024-08-23 23:45:40 +0100

    update scripts, add dhs estimate

--- 07_extract_data.R
+++ 07_extract_data.R
@@ -1,4 +1,4 @@
-##Extract envrionmental variables for Warri and Asaba
+##Extract environmental variables for Warri and Asaba
 
 library(raster)
 library(sf)
@@ -9,6 +9,7 @@ library(dplyr)
 library(purrr)
 library(haven)
 library(stars)
+library(gridExtra)
 
 
 #Load directories
@@ -16,11 +17,12 @@ Drive <- file.path(gsub("[\\]", "/", gsub("Documents", "", gsub("OneDrive", "", 
 Drive <- file.path(gsub("[//]", "/", Drive))
 DriveDir <- file.path(Drive, "Urban Malaria Proj Dropbox", "urban_malaria")
 RasterDir <- file.path(DriveDir, "data", "nigeria", "Raster_files")
+TPRDir <- file.path(DriveDir, "data", "nigeria", "TPR")
+PfDir <- file.path(RasterDir, "Malaria Atlas")
+ShpDir <- file.path(DriveDir, "data", "nigeria", "shapefiles", "ShinyApp_shapefiles")
 
 
 ## Load shapefiles
-ShpDir <- file.path(DriveDir, "data", "nigeria", "shapefiles", "ShinyApp_shapefiles")
-
 warri_shp <- st_read(file.path(ShpDir, "Warri", "Warri.shp"))
 asaba_shp <- st_read(file.path(ShpDir, "Asaba", "Asaba.shp"))
 
@@ -120,6 +122,7 @@ asaba_rh <- extract(relative_humidity, asaba_shp, fun = mean, df = TRUE)
 colnames(asaba_rh)[2] <- "relative_humidity"
 asaba_shp$relative_humidity <- asaba_rh$relative_humidity
 
+
 #### temperature
 temperature <- raster(file.path(RasterDir, "temperature_2024.grib"))
 
@@ -165,5 +168,316 @@ saveRDS(asaba_variables, "asaba_var.RDS")
 
 write.csv(asaba_variables, file.path(ShpDir, "asaba_variables.csv"))
 
-  
+
+#read summarized variables
+warri_variables <- readRDS("warri_var.RDS")
+asaba_variables <- readRDS("asaba_var.RDS")
+
+
+### Relative humidity 2
+
+list_RH <- list(rh_2023 <- brick(file.path(RasterDir, "relative_humidity_2023.grib")),
+                rh_2024 <- brick(file.path(RasterDir, "relative_humidity_2024.grib"))
+)
+
+names(list_RH) <- c("2023", "2024")
+
+for (i in 1:length(list_RH)) {
+  num_layers <- nlayers(list_RH[[i]])
+  layer_names <- paste0("RH_", seq_len(num_layers))
+  names(list_RH[[i]]) <- layer_names
+}
+
+
+nlayers(list_RH[[2]])
+plot(list_RH[[2]], 7)
+
+#extract for relative humidity for warri
+
+warri_list_RH <- list() 
+
+for (i in 1:length(list_RH)) {
+  warri_RH_data <- extract(list_RH[[i]], warri_shp, fun = mean, df = TRUE)
+  df_RH <- as.data.frame(warri_RH_data)
+  df_RH$Year <- names(list_RH)[i]
+  warri_list_RH[[i]] <- df_RH
+}
+
+warri_rh2 <- bind_rows(warri_list_RH)
+
+rh_warri <- warri_rh2 %>%
+  group_by(ID) %>%
+  summarize(RH_mean = mean(c_across(starts_with("RH_")), na.rm = TRUE))
+
+warri_variables$RH_mean <- rh_warri$RH_mean
+
+
+#extract for relative humidity for asaba
+
+asaba_list_RH <- list()
+
+for (i in 1:length(list_RH)) {
+  asaba_RH_data <- extract(list_RH[[i]], asaba_shp, fun = mean, df = TRUE)
+  df_RH <- as.data.frame(asaba_RH_data)
+  df_RH$Year <- names(list_RH)[i]
+  asaba_list_RH[[i]] <- df_RH
+}
+
+asaba_rh2 <- bind_rows(asaba_list_RH)
+
+rh_asaba <- asaba_rh2 %>%
+  group_by(ID) %>%
+  summarize(RH_mean = mean(c_across(starts_with("RH_")), na.rm = TRUE))
+
+asaba_variables$RH_mean <- rh_asaba$RH_mean
+
+
+#Temperature 2
+
+list_temp <- list(temp_2023 <- brick(file.path(RasterDir, "temperature_2023.grib")),
+                temp_2024 <- brick(file.path(RasterDir, "temperature_2024.grib"))
+)
+
+names(list_temp) <- c("2023", "2024")
+
+for (i in 1:length(list_temp)) {
+  num_layers <- nlayers(list_temp[[i]])
+  layer_names <- paste0("temp_", seq_len(num_layers))
+  names(list_temp[[i]]) <- layer_names
+}
+
+
+nlayers(list_temp[[2]])
+plot(list_temp[[2]], 7)
+
+#extract for temperature for warri
+
+warri_list_temp <- list() 
+
+for (i in 1:length(list_temp)) {
+  warri_temp_data <- extract(list_temp[[i]], warri_shp, fun = mean, df = TRUE)
+  df_temp <- as.data.frame(warri_temp_data)
+  df_temp$Year <- names(list_temp)[i]
+  warri_list_temp[[i]] <- df_temp
+}
+
+warri_temp2 <- bind_rows(warri_list_temp)
+
+temp_warri <- warri_temp2 %>%
+  group_by(ID) %>%
+  summarize(temp_mean = mean(c_across(starts_with("temp_")), na.rm = TRUE))
+
+warri_variables$temp_mean <- temp_warri$temp_mean
+
+
+#extract for temperature for asaba
+
+asaba_list_temp <- list()
+
+for (i in 1:length(list_temp)) {
+  asaba_temp_data <- extract(list_temp[[i]], asaba_shp, fun = mean, df = TRUE)
+  df_temp <- as.data.frame(asaba_temp_data)
+  df_temp$Year <- names(list_temp)[i]
+  asaba_list_temp[[i]] <- df_temp
+}
+
+asaba_temp2 <- bind_rows(asaba_list_temp)
+
+temp_asaba <- asaba_temp2 %>%
+  group_by(ID) %>%
+  summarize(temp_mean = mean(c_across(starts_with("temp_")), na.rm = TRUE))
+
+asaba_variables$temp_mean <- temp_asaba$temp_mean
+
+
+
+### Plot variables
+
+#WARRI
+warri_evi <-
+  ggplot()+
+  geom_sf(data = warri_variables, aes(geometry = geometry, fill = mean_EVI))+
+  scale_fill_continuous(low = "lightyellow", high = "darkgreen", na.value = "grey")+
+  #geom_text_repel(data = umuagu_plot %>% 
+   #                 filter(FID == "0"), aes(geometry = geometry, label = FID),
+    #              stat = "sf_coordinates", size = 3, min.segment.length = 0) +
+  labs(title = "2023- 2024 Mean EVI",
+       x = "",
+       y = "",
+       fill = "EVI" )+
+  map_theme()
+
+warri_ndvi <-
+  ggplot()+
+  geom_sf(data = warri_variables, aes(geometry = geometry, fill = mean_NDVI))+
+  scale_fill_continuous(low = "lightyellow", high = "darkgreen", na.value = "grey")+
+  labs(title = "2023- 2024 Mean NDVI",
+       x = "",
+       y = "",
+       fill = "NDVI" )+
+  map_theme()
+
+warri_rain <-
+  ggplot()+
+  geom_sf(data = warri_variables, aes(geometry = geometry, fill = mean_rainfall))+
+  scale_fill_continuous(low = "lightblue", high = "darkblue", na.value = "grey")+
+  labs(title = "2023- 2024 Mean Rainfall",
+       x = "",
+       y = "",
+       fill = "Rainfall (mm)" )+
+  map_theme()
+
+warri_humidity <-
+  ggplot()+
+  geom_sf(data = warri_variables, aes(geometry = geometry, fill = RH_mean))+
+  scale_fill_continuous(low = "lightyellow", high = "brown", na.value = "grey")+
+  labs(title = "2023- 2024 Relative Humidity",
+       x = "",
+       y = "",
+       fill = "Relative Humidity %" )+
+  map_theme()
+
+
+warri_h2o <-
+  ggplot()+
+  geom_sf(data = warri_variables, aes(geometry = geometry, fill = distance_to_water))+
+  scale_fill_continuous(low = "lightyellow", high = "purple", na.value = "grey")+
+  labs(title = "Distance to Water bodies",
+       x = "",
+       y = "",
+       fill = "" )+
+  map_theme()
+
+warri_temp <-
+  ggplot()+
+  geom_sf(data = warri_variables, aes(geometry = geometry, fill = temp_mean))+
+  scale_fill_continuous(low = "lightyellow", high = "maroon", na.value = "grey")+
+  labs(title = "Mean Temperature",
+       x = "",
+       y = "",
+       fill = "Temperature °C" )+
+  map_theme()
+
+grid.arrange(warri_evi, warri_ndvi, warri_rain, warri_humidity, warri_h2o, warri_temp, ncol = 2)
+
+
+#ASABA
+
+asaba_evi <-
+  ggplot()+
+  geom_sf(data = asaba_variables, aes(geometry = geometry, fill = mean_EVI))+
+  scale_fill_continuous(low = "lightyellow", high = "darkgreen", na.value = "grey")+
+  #geom_text_repel(data = umuagu_plot %>% 
+  #                 filter(FID == "0"), aes(geometry = geometry, label = FID),
+  #              stat = "sf_coordinates", size = 3, min.segment.length = 0) +
+  labs(title = "2023- 2024 Mean EVI",
+       x = "",
+       y = "",
+       fill = "EVI" )+
+  map_theme()
+
+asaba_ndvi <-
+  ggplot()+
+  geom_sf(data = asaba_variables, aes(geometry = geometry, fill = mean_NDVI))+
+  scale_fill_continuous(low = "lightyellow", high = "darkgreen", na.value = "grey")+
+  labs(title = "2023- 2024 Mean NDVI",
+       x = "",
+       y = "",
+       fill = "NDVI" )+
+  map_theme()
+
+asaba_rain <-
+  ggplot()+
+  geom_sf(data = asaba_variables, aes(geometry = geometry, fill = mean_rainfall))+
+  scale_fill_continuous(low = "lightblue", high = "darkblue", na.value = "grey")+
+  labs(title = "2023- 2024 Mean Rainfall",
+       x = "",
+       y = "",
+       fill = "Rainfall (mm)" )+
+  map_theme()
+
+asaba_humidity <-
+  ggplot()+
+  geom_sf(data = asaba_variables, aes(geometry = geometry, fill = RH_mean))+
+  scale_fill_continuous(low = "lightyellow", high = "brown", na.value = "grey")+
+  labs(title = "2023- 2024 Relative Humidity",
+       x = "",
+       y = "",
+       fill = "Relative Humidity %" )+
+  map_theme()
+
+
+asaba_h2o <-
+  ggplot()+
+  geom_sf(data = asaba_variables, aes(geometry = geometry, fill = distance_to_water))+
+  scale_fill_continuous(low = "lightyellow", high = "purple", na.value = "grey")+
+  labs(title = "Distance to Water bodies",
+       x = "",
+       y = "",
+       fill = "" )+
+  map_theme()
+
+
+asaba_temp <-
+  ggplot()+
+  geom_sf(data = asaba_variables, aes(geometry = geometry, fill = temp_mean))+
+  scale_fill_continuous(low = "lightyellow", high = "maroon", na.value = "grey")+
+  labs(title = "Mean Temperature",
+       x = "",
+       y = "",
+       fill = "Temperature °C" )+
+  map_theme()
+
+grid.arrange(asaba_evi, asaba_ndvi, asaba_rain, asaba_humidity, asaba_h2o, asaba_temp, ncol = 3)
+
+
+
+warri_variables2 <- warri_variables %>% 
+  st_drop_geometry()
+write.csv(warri_variables2, file.path(ShpDir, "warri_variables.csv"))
+
+
+asaba_variables2 <- asaba_variables %>% 
+  st_drop_geometry()
+write.csv(asaba_variables2, file.path(ShpDir, "asaba_variables.csv"))
+
+
+# Extract pf parasite rate
+
+pfpr_2022 <- raster(file.path(PfDir, "pf_parasite_rate",
+                          "202406_Global_Pf_Parasite_Rate_NGA_2022.tiff"))
+
+warri_pfpr<- extract(pfpr_2022, warri_shp, fun = mean, df = TRUE)
+warri_variables$pfpr <- warri_pfpr$X202406_Global_Pf_Parasite_Rate_NGA_2022_1
+
+
+asaba_pfpr<- extract(pfpr_2022, asaba_shp, fun = mean, df = TRUE)
+asaba_variables$pfpr <- asaba_pfpr$X202406_Global_Pf_Parasite_Rate_NGA_2022_1
+
+#Include TPR values
+
+warri_tpr <- read.csv(file.path(TPRDir, "warri TPR.csv"))
+warri_variables <- left_join(
+  warri_variables, 
+  warri_tpr %>% select(WardName, tpr_u5, total_tpr), 
+  by = "WardName"
+)
+
+asaba_tpr <- read.csv(file.path(TPRDir, "Asaba TPR.csv"))
+asaba_variables <- left_join(
+  asaba_variables,
+  asaba_tpr %>% select(WardName, tpr_u5, total_tpr),
+  by = "WardName"
+)
+
+
+#housing quality
+housing_quality <- raster(file.path(RasterDir, "housing",
+                              "2019_Nature_Africa_Housing_2015_NGA.tiff"))
+
+warri_housing<- extract(housing_quality, warri_shp, fun = mean, df = TRUE)
+warri_variables$housing_quality <- warri_housing$X2019_Nature_Africa_Housing_2015_NGA
+
+asaba_housing<- extract(housing_quality, asaba_shp, fun = mean, df = TRUE)
+asaba_variables$housing_quality <- asaba_housing$X2019_Nature_Africa_Housing_2015_NGA
 
---
+++ 08_dhs_estimates.R
@@ -0,0 +1,125 @@
+#DHS Analysis for malaria prevalence
+
+library(raster)
+library(sf)
+library(terra)
+library(stringr)
+library(ggplot2)
+library(dplyr)
+library(purrr)
+library(haven)
+library(stars)
+library(gridExtra)
+
+library(tidyr)
+library(srvyr)
+library(sjlabelled)
+library(RColorBrewer)
+library(blscrapeR)
+library(readr)
+library(labelled)
+library(tidyverse)
+library(janitor)
+library(mapsf)
+library(readxl)
+library(survey)
+library(ggthemes)
+library(ggrepel)
+
+
+
+#Load directories
+Drive <- file.path(gsub("[\\]", "/", gsub("Documents", "", gsub("OneDrive", "", Sys.getenv("HOME")))))
+Drive <- file.path(gsub("[//]", "/", Drive))
+DriveDir <- file.path(Drive, "Urban Malaria Proj Dropbox", "urban_malaria")
+DHSDir <- file.path(DriveDir, "data", "dhs", "Downloads")
+ShpDir <- file.path(DriveDir, "data", "nigeria", "shapefiles", "ShinyApp_shapefiles")
+
+
+## Load shapefiles
+warri_shp <- st_read(file.path(ShpDir, "Warri", "Warri.shp"))
+asaba_shp <- st_read(file.path(ShpDir, "Asaba", "Asaba.shp"))
+
+
+### read .dta files
+read.files <- function(path, general_pattern, specific_pattern, fun) {
+  files <- list.files(path = path , pattern = general_pattern, full.names = TRUE, recursive = TRUE)
+  files<- files[(grep(specific_pattern, files))]
+  sapply(files, fun, simplify = F)
+}
+
+
+# Read in PR and KR data files and rename state variable
+pr_files <- read.files(DHSDir, "*NGPR.*\\.DTA", 'NGPR4CFL|NGPR53FL|NGPR61FL|NGPR6AFL|NGPR71FL|NGPR7AFL|NGPR81FL', read_dta)
+
+kr_files <- read.files(DHSDir, "*NGKR.*\\.DTA", 'NGKR4BFL|NGKR53FL|NGKR61FL|NGKR6AFL|NGKR71FL|NGKR7AFL|NGKR81FL', read_dta)
+
+
+pr_files[[5]]$state <- as_label(pr_files[[5]]$shstate)
+pr_files[[6]]$state <- as_label(pr_files[[6]]$shstate)
+pr_files[[7]]$state <- as_label(pr_files[[7]]$hv024)
+pr_data <- bind_rows(pr_files)
+
+pr_data_2021 <- pr_files[[7]]
+
+
+lapply(pr_files, function(x) table(x$hml32))
+
+
+## Load spatial points
+sf21 = sf::st_read(file.path(DHSDir, "NG_2021_MIS_12052022_1735_141460/NGGE81FL/NGGE81FL.shp"))
+sf18 = sf::st_read(file.path(DHSDir, "NG_2018_DHS_11072019_1720_86355/NGGE7BFL/NGGE7BFL.shp")) 
+sf15 = sf::st_read(file.path(DHSDir, "NG_2015_MIS_06192019/NGGE71FL/NGGE71FL.shp")) 
+sf_all = rbind(sf21, sf18, sf15) %>% rename(cluster = DHSCLUST)
+
+
+malaria_prev <- pr_data %>%
+  filter(hv042 == 1 & hv103 == 1 & hc1 %in% c(6:59) ) %>%
+  filter(hml32 < 6) %>% 
+  mutate(malaria = ifelse(hml32 == 1, 1, 0), wt = hv005/1000000, id  = hv021, strat=hv022) %>% 
+  srvyr:: as_survey_design(., ids = id, strata = strat,nest = T, weights = wt) %>%
+  group_by(state, cluster = hv001, year = hv007) %>% 
+  summarize(prev = round(survey_mean(malaria),2),
+             total_malaria = survey_total()) %>%
+  mutate(class= cut(prev,  c(seq(0, 20, 5),30,50, 100), include.lowest = T)) %>% 
+  filter(state == "delta") %>%
+  inner_join(sf_all, by = c("year" = "DHSYEAR","cluster" = "cluster" )) %>%
+  drop_na(prev)
+
+ggplot()+
+  geom_sf(data = malaria_prev, aes(geometry = geometry))
+
+#point malaria prevalence
+
+# Warri
+malaria_prev2 <- sf::st_as_sf(malaria_prev, coords = c("LONGNUM", "LATNUM"))
+sf::st_crs(malaria_prev2) <-  sf::st_crs(warri_shp)
+warri_pr_data <- st_intersection(warri_shp, malaria_prev2)
+
+
+#Asaba
+
+malaria_prev3 <- sf::st_as_sf(malaria_prev, coords = c("LONGNUM", "LATNUM"))
+sf::st_crs(malaria_prev3) <-  sf::st_crs(asaba_shp)
+asaba_pr_data <- st_intersection(asaba_shp, malaria_prev3)
+
+ggplot()+
+  geom_sf(data = asaba_shp, aes(geometry = geometry))+
+  geom_sf(data = asaba_pr_data, aes(geometry = geometry))+
+  ggrepel::geom_text_repel(data = asaba_pr_data, aes(label = prev, geometry = geometry),
+                           color = "black", stat = "sf_coordinates",
+                           min.segment.length = 0, size = 4, force = 1)+
+  labs(title = "Malaria point prevalence Asaba",
+       x = "", y = "",
+       caption = "Data only available for Umuagu")+
+  map_theme()
+
+
+
+
+
+
+
+
+
+
