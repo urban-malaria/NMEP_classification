a619653f7aab14132c53678becc3f59fa93c6c26
Grace Legris <gracebea@gmail.com>
2025-04-25 15:16:31 -0500

    testing removing 30% threshold

--- all_reprior_scenarios_no_threshold.R
+++ all_reprior_scenarios_no_threshold.R
@@ -20,53 +20,6 @@ niger_variables <- read.csv(file.path(OutputsDir, "Final Extractions", "niger_pl
     classification_75 = ifelse(urbanPercentage > 75, "Urban", "Rural")
   )
 
-# load and clean itn data for niger
-niger_itn_data <- readxl::read_excel(
-  file.path(ITNDir, "pbi_distribution_Niger.xlsx"), 
-  sheet = 1) 
-
-niger_itn_clean <- niger_itn_data %>%
-  rename(population = `Sum of N_Nets`,
-         Ward = `Row Labels`) %>%
-  dplyr::select(population, Ward) %>%
-  group_by(Ward) %>%
-  summarise(Population = sum(population, na.rm = T)) %>%
-  ungroup() %>%
-  mutate(Ward = case_when(
-    Ward == "Adunu" ~ "Adono",
-    Ward == "Akare" ~ "Akari",
-    Ward == "Anaba" ~ "Anaba/Ibelu West",
-    Ward == "Badeggi" ~ "Badegi",
-    Ward == "Bagama B" ~ "Bagama b",
-    Ward == "Bonu" ~ "Bono",
-    Ward == "Chanchaga" ~ "Chachanga",
-    Ward == "Cheniyan" ~ "Chenian",
-    Ward == "Ciki Gari" ~ "Cikin Gari",
-    Ward == "Danrangi" ~ "Danragi",
-    Ward == "Dokko" ~ "Doko",
-    Ward == "Dokodza" ~ "Dokoza",
-    Ward == "Ebbo/Gbachinku" ~ "Ebbo",
-    Ward == "Ecwa/Gwada" ~ "Egwa/Gwada",
-    Ward == "Edozhigi" ~ "Edozigi",
-    Ward == "Fahzi" ~ "Fazhi",
-    Ward == "Gazhe" ~ "Gazhe 1",
-    Ward == "Gupa/Abugi" ~ "Gupa",
-    Ward == "Gwarjiko" ~ "Gwarijiko",
-    Ward == "Kafin Koro" ~ "Kafinkoro",
-    Ward == "Kuchi Bussu" ~ "Kucibusu",
-    Ward == "Kura" ~ "Kura/Auna East",
-    Ward == "Kurebe/Kushaka" ~ "Kuregbe",
-    Ward == "Kuso Tachin" ~ "Kusotacin",
-    Ward == "Labohzi" ~ "Labozhi",
-    Ward == "Lokogoma" ~ "Lokogwoma",
-    Ward == "Magamadaji" ~ "Magamadaji/Ibelu North",
-    Ward == "Mantabefyan" ~ "Manbe Tafyan",
-    Ward == "Moregi" ~ "Muregi",
-    Ward == "Muye" ~ "Muye/Egba",
-    TRUE ~ Ward  # Keep original value if no match
-  )) %>%
-  mutate(num = 1:n())
-
 niger_itn_clean <- read_excel(file.path("/Users/grace/Urban Malaria Proj Dropbox/urban_malaria/data/nigeria/ITN_distribution/household_member_ward_summaries_Itn_distribution/ITN_distribution_total_ward_niger_2022.xlsx"))
 
 niger_itn_clean <- niger_itn_clean %>%
@@ -91,19 +44,21 @@ prioritized_niger_3 <- prioritize_wards(
   population_col = "Population", 
   rank_col = "ranks", 
   class_col = "classification_50", 
-  ward_col = "WardName.x"
+  ward_col = "WardName.x",
+  target_percentage = 20
 )
 prioritized_niger_4 <- prioritize_wards(
   data = combined_wards2_ni, 
   population_col = "Population", 
   rank_col = "ranks", 
   class_col = "classification_75", 
-  ward_col = "WardName.x"
+  ward_col = "WardName.x",
+  target_percentage = 30
 )
 
 # save prioritized wards
-write.csv(prioritized_niger_3, file.path(OutputsDir, "NMEP Presentation Reprioritization Tables", "Niger", "niger_scenario3_no_threshold.csv"))
-write.csv(prioritized_niger_4, file.path(OutputsDir, "NMEP Presentation Reprioritization Tables", "Niger", "niger_scenario4_no_threshold.csv"))
+# write.csv(prioritized_niger_3, file.path(OutputsDir, "NMEP Presentation Reprioritization Tables", "Niger", "niger_scenario3_no_threshold.csv"))
+# write.csv(prioritized_niger_4, file.path(OutputsDir, "NMEP Presentation Reprioritization Tables", "Niger", "niger_scenario4_no_threshold.csv"))
 
 ## =========================================================================================================================================
 ### KANO
@@ -125,19 +80,15 @@ kano_variables <- read.csv(file.path(OutputsDir, "Final Extractions", "kano_plus
 # load rankings for kano
 kano_ranks <- read.csv(file.path(OutputsDir, "rankings", "Kano_rankings.csv"))
 
-# load and clean itn data for kano
-kano_itn_data <- readxl::read_excel(
-  file.path(ITNDir, "pbi_distribution_Kano.xlsx"), 
-  sheet = 2) 
+# load ITN data for kano
+kano_itn_clean <- read_excel(file.path("/Users/grace/Urban Malaria Proj Dropbox/urban_malaria/data/nigeria/ITN_distribution/household_member_ward_summaries_Itn_distribution/ITN_distribution_total_ward_kano_2022.xlsx"))
 
-kano_itn_clean <- kano_itn_data %>% 
-  rename(population = N_FamilyMembers,
-         Ward = AdminLevel3) %>% 
-  dplyr::select(population, Ward) %>% 
-  group_by(Ward) %>% 
-  summarise(Population = sum(population, na.rm =T)) %>% 
-  ungroup() %>% 
-  mutate(num = 1:n())
+kano_itn_clean <- kano_itn_clean %>%
+  rename(population = `Sum of N_FamilyMembers`) %>%
+  dplyr::select(population, Ward) %>%
+  group_by(Ward) %>%
+  summarise(Population = sum(population, na.rm = T)) %>%
+  ungroup()
 
 # merge datasets for kano
 combined_wards2_kn <- kano_variables %>% 
@@ -151,19 +102,21 @@ prioritized_kano_3 <- prioritize_wards(
   population_col = "Population", 
   rank_col = "ranks", 
   class_col = "classification_50", 
-  ward_col = "WardName.x"
+  ward_col = "WardName.x",
+  target_percentage = 30
 )
 prioritized_kano_4 <- prioritize_wards(
   data = combined_wards2_kn, 
   population_col = "Population", 
   rank_col = "ranks", 
   class_col = "classification_75", 
-  ward_col = "WardName.x"
+  ward_col = "WardName.x",
+  target_percentage = 30
 )
 
 # save prioritized wards
-write.csv(prioritized_kano_3, file.path(OutputsDir, "NMEP Presentation Reprioritization Tables", "Kano", "kano_scenario3_no_threshold.csv"))
-write.csv(prioritized_kano_4, file.path(OutputsDir, "NMEP Presentation Reprioritization Tables", "Kano", "kano_scenario4_no_threshold.csv"))
+# write.csv(prioritized_kano_3, file.path(OutputsDir, "NMEP Presentation Reprioritization Tables", "Kano", "kano_scenario3_no_threshold.csv"))
+# write.csv(prioritized_kano_4, file.path(OutputsDir, "NMEP Presentation Reprioritization Tables", "Kano", "kano_scenario4_no_threshold.csv"))
 
 ## =========================================================================================================================================
 ### KATSINA
@@ -187,16 +140,15 @@ katsina_variables <- read.csv(file.path(OutputsDir, "Final Extractions", "katsin
 katsina_ranks <- read.csv(file.path(OutputsDir, "rankings", "Katsina_rankings.csv")) %>%
   dplyr::mutate(WardName = str_trim(WardName), ranks = str_trim(ranks))
 
-# read in ITN data
-katsina_itn_data <- readxl::read_excel(file.path(ITNDir, "pbi_distribution_Katsina.xlsx"))
+# load ITN data for Katsina
+katsina_itn_clean <- read_excel(file.path("/Users/grace/Urban Malaria Proj Dropbox/urban_malaria/data/nigeria/ITN_distribution/household_member_ward_summaries_Itn_distribution/ITN_distribution_total_ward_katsina_2022.xlsx"))
 
-# clean ITN data
-katsina_itn_clean <- katsina_itn_data %>% 
-  rename(population = N_FamilyMembers,
-         Ward = AdminLevel3) %>% 
-  dplyr::select(population, Ward) %>% 
-  group_by(Ward) %>% 
-  summarise(Population = sum(population, na.rm =T))
+katsina_itn_clean <- katsina_itn_clean %>%
+  rename(population = `Sum of N_FamilyMembers`) %>%
+  dplyr::select(population, Ward) %>%
+  group_by(Ward) %>%
+  summarise(Population = sum(population, na.rm = T)) %>%
+  ungroup()
 
 
 # combine datasets
@@ -208,18 +160,76 @@ prioritized_katsina_50 <- prioritize_wards(data = combined_wards2,
                                            population_col = "Population", 
                                            rank_col = "ranks", 
                                            class_col = "classification_50", 
-                                           ward_col = "WardName")
+                                           ward_col = "WardName",
+                                           target_percentage = 30)
 
 # run reprioritization function - CLASSIFICATION 75
 prioritized_katsina_75 <- prioritize_wards(data = combined_wards2, 
                                            population_col = "Population", 
                                            rank_col = "ranks", 
                                            class_col = "classification_75", 
-                                           ward_col = "WardName")
+                                           ward_col = "WardName",
+                                           target_percentage = 30)
 
 # save reprioritization tables
-write.csv(prioritized_katsina_50, file.path(OutputsDir, "NMEP Presentation Reprioritization Tables", "Katsina/", "katsina_scenario_3_no_threshold.csv"))
-write.csv(prioritized_katsina_75, file.path(OutputsDir, "NMEP Presentation Reprioritization Tables", "Katsina/", "katsina_scenario_4_no_threshold.csv"))
+# write.csv(prioritized_katsina_50, file.path(OutputsDir, "NMEP Presentation Reprioritization Tables", "Katsina/", "katsina_scenario_3_no_threshold.csv"))
+# write.csv(prioritized_katsina_75, file.path(OutputsDir, "NMEP Presentation Reprioritization Tables", "Katsina/", "katsina_scenario_4_no_threshold.csv"))
+
+## =========================================================================================================================================
+### KADUNA
+## =========================================================================================================================================
+
+# load shapefile
+kaduna_shp <- st_read(file.path(StateShpDir, "Kaduna", "Kaduna_State.shp"))
+
+# load and clean variables
+kaduna_variables <- read.csv(file.path(OutputsDir, "Final Extractions", "kaduna_plus.csv")) %>% 
+  distinct(WardCode, .keep_all = TRUE) %>%
+  dplyr::select(X, WardName.x, urbanPercentage, WardCode) %>% 
+  mutate(
+    classification_20 = ifelse(urbanPercentage > 20, "Urban", "Rural"),
+    classification_30 = ifelse(urbanPercentage > 30, "Urban", "Rural"),
+    classification_50 = ifelse(urbanPercentage > 50, "Urban", "Rural"),
+    classification_75 = ifelse(urbanPercentage > 75, "Urban", "Rural")
+  )
+
+# read in rankings
+kaduna_ranks <- read.csv(file.path(OutputsDir, "rankings", "Kaduna_rankings.csv")) %>%
+  dplyr::mutate(WardName = str_trim(WardName), ranks = str_trim(ranks))
+
+# load ITN data for Katsina
+kaduna_itn_clean <- read_excel(file.path("/Users/grace/Urban Malaria Proj Dropbox/urban_malaria/data/nigeria/ITN_distribution/household_member_ward_summaries_Itn_distribution/ITN_distribution_total_ward_kaduna_2022.xlsx"))
+
+kaduna_itn_clean <- kaduna_itn_clean %>%
+  rename(population = `Sum of N_FamilyMembers`) %>%
+  dplyr::select(population, Ward) %>%
+  group_by(Ward) %>%
+  summarise(Population = sum(population, na.rm = T)) %>%
+  ungroup()
+
+# combine datasets
+combined_wards <- left_join(kaduna_variables, kaduna_ranks, by = "WardCode")
+combined_wards2 <- left_join(combined_wards, kaduna_itn_clean, by = c("WardName.x" = "Ward")) 
+
+# run reprioritization function - CLASSIFICATION 50
+prioritized_kaduna_50 <- prioritize_wards(data = combined_wards2, 
+                                           population_col = "Population", 
+                                           rank_col = "ranks", 
+                                           class_col = "classification_50", 
+                                           ward_col = "WardName",
+                                           target_percentage = 30)
+
+# run reprioritization function - CLASSIFICATION 75
+prioritized_kaduna_75 <- prioritize_wards(data = combined_wards2, 
+                                           population_col = "Population", 
+                                           rank_col = "ranks", 
+                                           class_col = "classification_75", 
+                                           ward_col = "WardName",
+                                           target_percentage = 30)
+
+# save reprioritization tables
+# write.csv(prioritized_katsina_50, file.path(OutputsDir, "NMEP Presentation Reprioritization Tables", "Katsina/", "katsina_scenario_3_no_threshold.csv"))
+# write.csv(prioritized_katsina_75, file.path(OutputsDir, "NMEP Presentation Reprioritization Tables", "Katsina/", "katsina_scenario_4_no_threshold.csv"))
 
 ## =========================================================================================================================================
 ### TARABA
@@ -243,47 +253,15 @@ taraba_variables <- read.csv(file.path(OutputsDir, "Final Extractions", "taraba_
 taraba_ranks <- read.csv(file.path(OutputsDir, "rankings", "Taraba_rankings.csv")) %>%
   dplyr::mutate(WardName = str_trim(WardName), ranks = str_trim(ranks))
 
-# read in ITN data
-taraba_itn_data <- readxl::read_excel(file.path(ITNDir, "pbi_distribution_Taraba.xlsx"))
+# load ITN data for Taraba
+taraba_itn_clean <- read_excel(file.path("/Users/grace/Urban Malaria Proj Dropbox/urban_malaria/data/nigeria/ITN_distribution/household_member_ward_summaries_Itn_distribution/ITN_distribution_total_ward_taraba_2022.xlsx"))
 
-# clean ITN data
-taraba_itn_clean <- taraba_itn_data %>%
-  rename(Ward = `Row Labels`, num_nets = `Sum of N_Nets`) %>%
-  dplyr::select(Ward, num_nets) %>%
+taraba_itn_clean <- taraba_itn_clean %>%
+  rename(population = `Sum of N_FamilyMembers`) %>%
+  dplyr::select(population, Ward) %>%
   group_by(Ward) %>%
-  summarise(num_nets = sum(num_nets, na.rm = TRUE)) %>%
-  mutate(Ward = case_when(
-    Ward == "Dampar I" ~ "Dampar 1",
-    Ward == "Dampar II" ~ "Dampar 2",
-    Ward == "Dampar III" ~ "Dampar 3",
-    Ward == "Lissam I" ~ "Lissam 1",
-    Ward == "Lissam Ii" ~ "Lissam 2",
-    Ward == "Nwonyo I" ~ "Nwonyo 1",
-    Ward == "Nwonyo II" ~ "Nwonyo 2",
-    Ward == "Rimi Uku I" ~ "Rimi Uku 1",
-    Ward == "Rimi Uku II" ~ "Rimi Uku 2",
-    Ward == "Sarkin Kudu I" ~ "Sarkin Kudu 1",
-    Ward == "Sarkin Kudu II" ~ "Sarkin Kudu 2",
-    Ward == "Sarkin Kudu III" ~ "Sarkin Kudu 3",
-    Ward == "Kussum Badakoshi" ~ "Kussum/Badakoshi",
-    Ward == "Panti-Sawa" ~ "Pantisawa",
-    Ward == "Zangonkombi" ~ "Zangon Kombi",
-    Ward == "Kassala-Sembe" ~ "Kachala-Sembe",
-    Ward == "Kpambo Purfi" ~ "Kpambo Puri",
-    Ward == "Kotsensi" ~ "Kosensi",
-    Ward == "Mbamaga" ~ "Mbamnga",
-    Ward == "Gangdole" ~ "Gang Dole",
-    Ward == "Ganglari" ~ "Gang Lari",
-    Ward == "Gangmata" ~ "Gang Mata", 
-    Ward == "Gangtiba" ~ "Gang Tiba",
-    Ward == "Jenkaigama" ~ "Jen Kaigama",
-    Ward == "Wurojam" ~ "Wuro Jam",
-    TRUE ~ Ward
-  )) %>%
-  filter(Ward != "(blank)" & 
-           Ward != "Grand Total" & 
-           !is.na(Ward) & 
-           Ward != "")
+  summarise(Population = sum(population, na.rm = T)) %>%
+  ungroup()
 
 # combine datasets
 combined_wards <- left_join(taraba_variables, taraba_ranks, by = "WardCode")
@@ -291,21 +269,23 @@ combined_wards2 <- left_join(combined_wards, taraba_itn_clean, by = c("WardName.
 
 # run reprioritization function - CLASSIFICATION 50
 prioritized_taraba_50 <- prioritize_wards(data = combined_wards2,
-                                          population_col = "num_nets",
+                                          population_col = "Population",
                                           rank_col = "ranks",
                                           class_col = "classification_50",
-                                          ward_col = "WardName.x")
+                                          ward_col = "WardName.x",
+                                          target_percentage = 30)
 
 # run reprioritization function - CLASSIFICATION 75
 prioritized_taraba_75 <- prioritize_wards(data = combined_wards2,
-                                          population_col = "num_nets",
+                                          population_col = "Population",
                                           rank_col = "ranks",
                                           class_col = "classification_75",
-                                          ward_col = "WardName.x")
+                                          ward_col = "WardName.x",
+                                          target_percentage = 30)
 
 # save reprioritization tables
-write.csv(prioritized_taraba_50, file.path(OutputsDir, "NMEP Presentation Reprioritization Tables", "Taraba/", "taraba_scenario_3_no_threshold.csv"))
-write.csv(prioritized_taraba_75, file.path(OutputsDir, "NMEP Presentation Reprioritization Tables", "Taraba/", "taraba_scenario_4_no_threshold.csv"))
+# write.csv(prioritized_taraba_50, file.path(OutputsDir, "NMEP Presentation Reprioritization Tables", "Taraba/", "taraba_scenario_3_no_threshold.csv"))
+# write.csv(prioritized_taraba_75, file.path(OutputsDir, "NMEP Presentation Reprioritization Tables", "Taraba/", "taraba_scenario_4_no_threshold.csv"))
 
 
 ## =========================================================================================================================================
@@ -339,22 +319,15 @@ yobe_ranks$WardName <- stringr::str_trim(yobe_ranks$WardName)
 yobe_ranks <- yobe_ranks %>% 
   mutate(num = row_number())
 
-# load ITN distribution data
-itn_dir <- file.path(DataDir, "nigeria/ITN_distribution")
-yobe_itn_data <- readxl::read_excel(
-  file.path(itn_dir, "pbi_distribution_Yobe.xlsx"), 
-  sheet = 1
-)
+# load ITN data for Yobe
+yobe_itn_clean <- read_excel(file.path("/Users/grace/Urban Malaria Proj Dropbox/urban_malaria/data/nigeria/ITN_distribution/household_member_ward_summaries_Itn_distribution/ITN_distribution_total_ward_yobe_2022_recoded.xlsx"))
 
-# clean ITN data
-yobe_itn_clean <- yobe_itn_data %>% 
-  rename(population = N_FamilyMembers,
-         Ward = AdminLevel3) %>% 
-  dplyr::select(population, Ward) %>% 
-  group_by(Ward) %>% 
-  summarise(Population = sum(population, na.rm = TRUE)) %>% 
-  ungroup() %>% 
-  mutate(num = row_number())
+yobe_itn_clean <- yobe_itn_clean %>%
+  rename(population = `Sum of N_FamilyMembers`) %>%
+  dplyr::select(population, Ward) %>%
+  group_by(Ward) %>%
+  summarise(Population = sum(population, na.rm = T)) %>%
+  ungroup()
 
 # merge datasets
 combined_wards <- left_join(yobe_variables, yobe_ranks, by = "WardCode")
@@ -365,15 +338,17 @@ prioritized_yobe_50 <- prioritize_wards(data = combined_wards2,
                                           population_col = "Population",
                                           rank_col = "ranks",
                                           class_col = "classification_50",
-                                          ward_col = "WardName.x")
+                                          ward_col = "WardName.x",
+                                        target_percentage = 30)
 
 # run reprioritization function - CLASSIFICATION 75
 prioritized_yobe_75 <- prioritize_wards(data = combined_wards2,
                                           population_col = "Population",
                                           rank_col = "ranks",
                                           class_col = "classification_75",
-                                          ward_col = "WardName.x")
+                                          ward_col = "WardName.x",
+                                        target_percentage = 30)
 
 # save reprioritization tables
-write.csv(prioritized_yobe_50, file.path(OutputsDir, "NMEP Presentation Reprioritization Tables", "Yobe/", "yobe_scenario_3_no_threshold.csv"))
-write.csv(prioritized_yobe_75, file.path(OutputsDir, "NMEP Presentation Reprioritization Tables", "Yobe/", "yobe_scenario_4_no_threshold.csv"))
+# write.csv(prioritized_yobe_50, file.path(OutputsDir, "NMEP Presentation Reprioritization Tables", "Yobe/", "yobe_scenario_3_no_threshold.csv"))
+# write.csv(prioritized_yobe_75, file.path(OutputsDir, "NMEP Presentation Reprioritization Tables", "Yobe/", "yobe_scenario_4_no_threshold.csv"))
